7a6d6e3731e6c21a5396d9466310615f46fa9e53
Todd Densmore <tdensmore@echo360.com>
2016-03-21 00:44:52 -0400

    Fixed hardcoded column size

--- README.md
+++ README.md
@@ -1,7 +1,95 @@
-PYTHON
+The goal of this excercise is to create a CSV to JSON converter using any helpful tools or libraries, with the exception of those designed to handle CSV parsing.
 
-BASH
+The input should be valid CSV and the output should be valid JSON. 
 
+* The CSV file will consist of multiple rows. 
+* The rows are newline-delimited and columns are comma-separated.  
+* The first row will always be a header row. 
+* All rows will have the same number of columns.
+* Entries can optionally be enclosed in double quotation marks, with quotation marks in the data denoted as "" inside of a quoted field (For example """This is a great idea,"" he said.").  
+* Any ascii characters, including newlines, can appear inside of quotations.
+
+The data will be transformed into a JSON output that consists of an array of objects.  Each object will be composed of fields with the names of the headers and values from the corresponding row. Numbers should translate to JSON numbers while anything else is a string.
+
+Example input CSV file:
+
+```
+ID, Name, Age, Favorite Color
+1, Bob, 23, Red
+2, Samuel, 99, Turquoise
+3, "Doe, John", 44, Orange
+4, Jimmy, 16, "bold, Bold, BOLD,
+BOOOOLDDD Blue"
+```
+
+Example output valid JSON file:
+
+```
+[
+  {
+    "ID": 1,
+    "Name": "Bob",
+    "Age": 23,
+    "Favorite Color": "Red"
+  },
+  {
+    "ID": 2,
+    "Name": "Samuel",
+    "Age": 99,
+    "Favorite Color": "Turquoise"
+  },
+  {
+    "ID": 3,
+    "Name": "\"Doe, John\"",
+    "Age": 44,
+    "Favorite Color": "Orange"
+  },
+  {
+    "ID": 4,
+    "Name": "Jimmy",
+    "Age": 16,
+    "Favorite Color": "\"bold, Bold, BOLD,\nBOOOOLDDD Blue\""
+  }
+]
+```
+
+Solution:
+
+This solution uses Python 2.7.11 and is coded in the file: `parse.py`
+
+Two sample json files are included:
+
+`text1.csv`
+
+`text2.csv`
+
+Running the code:
+
+`> python parse.py`
+
+The user will be propmpted to enter the CSV file to parse. 
+
+`CSV file to parse [text1.csv]:`
+
+Hitting enter/return will use the default file: `text1.csv`
+
+The output JSON file is printed to standard out, and also to a file called 'output.json'
+
+NOTES:
+
+* The most common way to read in a file from Python is probably not going to work well here.
+* Proper escaping of newlines and quotes is very important
+* Parsing is hard to do with Python lists.
+
+FURTHER IMPROVEMENTS:
+
+* Improved error handling
+* Additional command line arguments
+* Input file validation (check and handle CSV format problems)
+* Improved JSON formatting
+* Improved modularity (split file reading / writing into own functions)
+* Ability to specify other output formats
+* Import other helpful utility libraries (command line args, logging, etc.)
 
 
 
--- parse.py
+++ parse.py
@@ -1,33 +1,40 @@
 # get filename to parse or use default
-filename = raw_input("CSV file to parse:") or "text1.csv"
+filename = raw_input("CSV file to parse [text1.csv]:") or "text1.csv"
 
+# the list to incremntally build the JSON object
 json_list = [ "[" ]
+
+# the CSV header row used to lookup the column names
 header_list = None
+
+# keep track of which CSV colum we are parsing
 number_of_csv_columns = None
+
+# whether to print lots of debug statements
 debug = True
 
+# function to build the JSON list (called from the parse loop)
 def build_json( column_index, value, is_end_of_file ):
 
     # get the correct column header and remove any whitespace
     column_string = ( header_list[column_index] ).strip()
-    if debug: print "calling write_json with column: {}  and value: {}".format(column_index,value)
+    if debug: print "calling write_json with column: {}  and value: {}".format( column_index, column_string )
 
-    # dont add a comma to the end of the JSON
+    # dont add a comma to the end of the JSON when we are parsing the last item
     last_character = ','
     if is_end_of_file:
         last_character = ''
-
     if column_index == 0:
         begin = '{'
         end = ','
-    elif column_index == 3:
+    elif column_index == number_of_csv_columns-1:
         begin = ''
         end = '}' + last_character
     else:
         begin = ''
         end = ','
 
-    # treat numbers differently in JSON
+    # create the JSON token, remembering to treat numbers differently in JSON
     token = ''
     if value.isdigit():
       token += begin + "\"" + column_string + "\": " + value + end
@@ -43,16 +50,26 @@ with open( filename, 'r') as file:
   list_of_lines = file.readlines()
   # get the first 'header' line and divide it into JSON keys
   header_list = ( list_of_lines.pop(0) ).split(",")
-  # get the number of keys to determine
+  # get the number of keys to determine the number of expected tokens per line
   number_of_csv_columns = len( header_list )
-  #print number_of_csv_columns
+  # convert the rest of the file to a string for parsing
   file_string = ''.join(list_of_lines)
 
+# function to keep track of which column we are on in the file
 def increment_column( column ):
     column += 1
     if column == number_of_csv_columns:
         column = 0
     return column
+
+# get the first line (header) of the file
+# tokenize the header to get the number of values
+# read the whole file in as a string
+
+# read each character to tokenize the string
+# the number of tokens depends on the number of CSV columns
+# do not split on commas inside quotes
+# preserve newlines inside quotes
 
 # if the character is a number, continue until a non number is found
 #     add the number string to the json list unquoted
@@ -78,11 +95,13 @@ token_string = ''
 # get each character in the CSV file string
 for idx, character_value in enumerate( file_string ):
 
-    print "{} : {}".format( idx,len( file_string ) )
+    #if debug: print "{} : {}".format( idx, len( file_string ) )
+
+    # check to see if we are on the last character of the file
     if idx + 1 == len( file_string ):
         is_end_of_file = True
 
-    # start to parse a digit
+    # start to parse chracters as a digit
     if character_value.isdigit():
         digit_started = True
         number_string += character_value
@@ -94,7 +113,7 @@ for idx, character_value in enumerate( file_string ):
         number_string = ''
         current_column = increment_column( current_column )
 
-    # SPACES
+    # handle SPACES
     elif character_value == ' ':
         if quote_started:
             if debug: print "space inside quotes"
@@ -103,7 +122,7 @@ for idx, character_value in enumerate( file_string ):
             if debug: print "space outside quotes: ", token_string
             continue
 
-    # NEWLINES
+    # handle NEWLINES
     elif character_value == '\n':
         if quote_started:
             if debug: print "newline inside quotes"
@@ -114,7 +133,7 @@ for idx, character_value in enumerate( file_string ):
             token_string = ''
             current_column = 0
 
-    # QUOTES
+    # handle QUOTES
     elif character_value == '"':
         if not quote_started:
             if debug: print "started quote"
@@ -127,7 +146,7 @@ for idx, character_value in enumerate( file_string ):
             token_string += '\\"'
             quote_started = False
 
-    # COMMAS
+    # handle COMMAS
     elif character_value == ",":
         if quote_started:
             if debug: print "found comma inside quotes"
@@ -138,11 +157,12 @@ for idx, character_value in enumerate( file_string ):
             current_column = increment_column( current_column )
             token_string = ''
 
-    # anything else
+    # handle everything else
     else:
         token_string += character_value
 
 json_list.append( "]" );
+print ''
 print ''.join(json_list)
 
 with open ("output.json", "w") as output_file:
